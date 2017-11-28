527c781ab4e0a9b6a34d827038750cf4a11b6346
Endirell <endirell@gmail.com>
2017-11-28 17:34:23 +0100

    Exercise4 ready

---
+++ week-02/day-1/Exercise26/Exercise26/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number 1: ");
+            string ask1 = Console.ReadLine();
+
+            int number1 = Convert.ToInt32(ask1);
+
+            Console.WriteLine("Number 2: ");
+            string ask2 = Console.ReadLine();
+
+            int number2 = Convert.ToInt32(ask2);
+
+            if (number1 >= number2)
+            {
+                Console.WriteLine("The second number should be bigger!");
+            }
+
+            else
+            {
+                int number3 = (number2 - number1);
+
+                for (int i = number1; i < number2; i++)
+
+                {          
+                    Console.WriteLine(number1++);
+                }
+            }
+
+            Console.ReadLine();
+            // Create a program that asks for two numbers
+            // If the second number is not bigger than the first one it should print:
+            // "The second number should be bigger"
+            //
+            // If it is bigger it should count from the first number to the second by one
+            //
+            // example:
+            //
+            // first number: 3, second number: 6, should print:
+            //
+            // 3
+            // 4
+            // 5
+        }
+    }
+}
--- week-02/day-2/Exercise04/Exercise04/Program.cs
+++ week-02/day-2/Exercise04/Exercise04/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Write a function called `sum` that sum all the numbers
+            //   until the given parameter
+            Console.WriteLine("Gimme a nummer fool!!!");
+            string nummer = Console.ReadLine();
+            int nummember = Convert.ToInt32(nummer);
+
+            Console.WriteLine(sum(nummember));
+
+            Console.ReadLine();
+        }
+        static int sum(int number)
+        {
+            return (number * (number + 1)) / 2;
+        }     
+    }
+}
