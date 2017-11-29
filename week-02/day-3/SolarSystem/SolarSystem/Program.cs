f7267c0839961d0206cd9062a7b9cc5cf32cfb9c
Endirell <endirell@gmail.com>
2017-11-29 17:35:17 +0100

    SolarSystem added

---
+++ week-02/day-3/SolarSystem/SolarSystem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolarSystem
+{
+    public class SolarSystem
+    {
+        public static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus",
+            "Neptune" };
+
+
+            // Saturn is missing from the planetList
+            // Insert it into the correct position
+
+            planetList.Insert(5, "Saturn");
+            planetList.Insert(8, "Nibiru");
+            foreach (string solarsystem in planetList)
+            
+            Console.WriteLine(solarsystem);
+            Console.ReadLine();
+        }
+        
+    }
+}
