3ce02adaf9da857a1608503df55e2ec2ce4ad38b
Endirell <endirell@gmail.com>
2017-12-01 16:00:38 +0100

    Guess my number not finished

--- week-02/day-5/Guessmynumber/Guessmynumber/Program.cs
+++ week-02/day-5/Guessmynumber/Guessmynumber/Program.cs
@@ -10,21 +10,36 @@ namespace Guessmynumber
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("I will guess from:");
             string range1s = Console.ReadLine();
             int range1 = Convert.ToInt32(range1s);
 
+            Console.WriteLine("to:");
             string range2s = Console.ReadLine();
             int range2 = Convert.ToInt32(range2s);
 
+            Console.WriteLine("How many lives you have?");
+            string lives = Console.ReadLine();
+            int life = Convert.ToInt32(lives);
 
             Random random = new Random();
             int number = random.Next(range1, (range2 +1));
 
-            Console.WriteLine(number);
+            Console.Clear();
+            Console.WriteLine("Guess a number from " + range1s + " to " + range2s + ". You have " + life + " life point");
 
+            
             Start:
 
+            
+
+            if (life == 0)
+            {
+                goto Lose;
+            }
+
             string guessstr = Console.ReadLine();
+                    
 
             int d;
             if (!int.TryParse(guessstr, out d))
@@ -39,16 +54,47 @@ namespace Guessmynumber
                 goto Wrong;
             }
 
-            goto Right;
+            if (guess == number)
+            {
+                goto Won;
+            }
 
+            if (guess < number)
+            {
+                Console.WriteLine("Too low, you have " + life + " life point");
+                int actuallife = (life - 1);
+                return actuallife;
+                goto Start;
+            }
+
+            if (guess > number)
+            {
+                Console.WriteLine("Too high, you have " + actuallife + " life point");                
+                goto Start;
+            }
+
+            else
+            {
+                goto Lose;
+            }
+            
             Wrong:
-            Console.WriteLine("\nWrong input! Type in numbers from 1-100\n");
+            Console.WriteLine("\nWrong input! Type in numbers from " +  range1s +  " to " + range2s + "\n");
             goto Start;
 
-            Right:
-
-            Console.WriteLine(guess);
+            
+            Won:
+            Console.WriteLine("You won!");
             Console.ReadLine();
+            goto Finish;
+
+            Lose:
+            Console.WriteLine("You have failed the Company and your Country!\nYou are pathetic! Kill yourself!");
+            Console.ReadLine();
+            goto Finish;
+
+            Finish:
+            Console.WriteLine("End");
 
 
 
