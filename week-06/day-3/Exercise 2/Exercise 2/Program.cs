f15d4742d94e18b9e4ab6da00311fb4418bcaa3c
Endirell <endirell@gmail.com>
2018-01-03 15:38:38 +0100

    Exercise 2

---
+++ week-06/day-3/Exercise 2/Exercise 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var oddNumbers = from number in numbers
+                              where (number % 2 != 0)
+                              orderby number descending
+                              select number;
+
+            double average = oddNumbers.Average(); 
+            
+            Console.WriteLine(average);
+
+            Console.ReadKey();
+
+        }
+    }
+}
