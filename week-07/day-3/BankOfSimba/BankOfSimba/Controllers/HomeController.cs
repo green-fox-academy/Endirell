1dc0acd956b9793819e1585cf1e77d5d41f1d10c
Endirell <endirell@gmail.com>
2018-01-10 15:16:36 +0100

    feat(BankOfSimba): Part I.

--- week-07/day-3/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
+++ week-07/day-3/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BankOfSimba.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,6 +15,14 @@ namespace BankOfSimba.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("simba")]
+        public IActionResult Simba()
+        {
+            BankAccount simba = new BankAccount("Simba", 2000, "Lion");
+
+            return View(simba);
         }
     }
 }
