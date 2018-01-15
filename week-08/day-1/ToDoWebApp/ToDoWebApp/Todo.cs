2367d8c83b7c9f98d3959ef06c93fb6989e09ee7
Endirell <endirell@gmail.com>
2018-01-15 13:33:46 +0100

    feat(ToDoWebApp): project created

--- week-08/day-1/ToDoWebApp/ToDoWebApp/Todo.cs
+++ week-08/day-1/ToDoWebApp/ToDoWebApp/Todo.cs
@@ -7,5 +7,12 @@ namespace ToDoWebApp
 {
     public class Todo
     {
+        //(Id:long, Title:string, IsUrgent:boolean(default false), IsDone:boolean(default false
+
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public bool IsUrgent { get; set; }
+        public bool IsDone { get; set; }
+
     }
 }
