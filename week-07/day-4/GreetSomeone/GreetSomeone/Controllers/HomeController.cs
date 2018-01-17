b51e5e2a6bf1d5a0eda6728ce2e853e0dbed102e
Endirell <endirell@gmail.com>
2018-01-17 17:15:43 +0100

    feat(GreetSomeone): Button works

---
+++ week-07/day-4/GreetSomeone/GreetSomeone/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GreetSomeone.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GreetSomeone.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        Greeting Visitor;
+
+        public HomeController(Greeting user)
+        {
+            Visitor = user;
+        }
+
+        [HttpGet("")]
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("greet")]
+        public IActionResult Greet()
+        {
+            return View(Visitor);
+        }
+
+        [HttpPost("addname")]
+        public IActionResult Addname(Greeting user)
+        {
+            Visitor.VisitorName = user.VisitorName;
+
+            return RedirectToAction("greet");
+        }
+    }
+}
