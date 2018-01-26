d5ccb91329106ca211558f28f4a1fae2dd11743f
Endirell <endirell@gmail.com>
2018-01-26 09:45:19 +0100

    feat(LicensePlatePractice): project created

--- z-Sideprojects/LicensePlatePractice/LicensePlatePractice/Program.cs
+++ z-Sideprojects/LicensePlatePractice/LicensePlatePractice/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace LicensePlacePractice
+namespace LicensePlatePractice
 {
     public class Program
     {
--- z-Sideprojects/LicensePlatePractice/LicensePlatePractice/Startup.cs
+++ z-Sideprojects/LicensePlatePractice/LicensePlatePractice/Startup.cs
@@ -6,22 +6,18 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using LicensePlacePractice.Entities;
-using Microsoft.EntityFrameworkCore;
-using LicensePlacePractice.Repositories;
 
-namespace LicensePlacePractice
+namespace LicensePlatePractice
 {
     public class Startup
     {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();            
-            services.AddDbContext<CarContext>((options => options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=cars;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")));
-            services.AddScoped<CarContext>();
-            services.AddScoped<CarRepository>();
         }
-        
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -29,7 +25,10 @@ namespace LicensePlacePractice
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
