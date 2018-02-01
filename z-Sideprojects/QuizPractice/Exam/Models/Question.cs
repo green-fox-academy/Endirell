bfd44ec1ca46bfb01208c0342be6761129180f8c
Endirell <endirell@gmail.com>
2018-02-01 11:29:54 +0100

    feat(Quiz): Model created

--- z-Sideprojects/QuizPractice/Exam/Models/Question.cs
+++ z-Sideprojects/QuizPractice/Exam/Models/Question.cs
@@ -8,6 +8,11 @@ namespace Quiz.Models
     public class Question
     {
         public long Id { get; set; }
-        public string String { get; set; }
+        public string AskQuestion { get; set; }
+        public string AnswerOne { get; set; }
+        public string AnswerTwo { get; set; }
+        public string AnswerThree { get; set; }
+        public string AnswerFour { get; set; }
+        public string TrueAnswer { get; set; }
     }
 }
